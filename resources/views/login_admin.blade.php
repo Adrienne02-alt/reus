5a9cfbe05e300a0e81e46ebd94adff800f28053e
Adrienne02-alt <adrhaingo@gmail.com>
2024-12-31 13:06:00 +0100

    Login

--- resources/views/login_admin.blade.php
+++ resources/views/login_admin.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Ticketing - Connexion</title>
   <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/Image 1.png') }}" />
-  <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css')  }}" />
+  <link rel="stylesheet" href="https://css-two-black.vercel.app/assets/css/styles.min.css" />
 </head>
 
 <body>
